28ad050af3950c9d46935d777d3aab071066e196
Timothé Louzier <louziert@gmail.com>
2021-11-13 13:04:18 +0100

    Spawning done

--- Assets/IA/BehaviourTree/Scripts/Actions/RandomPosition.cs
+++ Assets/IA/BehaviourTree/Scripts/Actions/RandomPosition.cs
@@ -7,6 +7,7 @@ public class RandomPosition : ActionNode
 {
     public Vector2 min = Vector2.one * -10;
     public Vector2 max = Vector2.one * 10;
+    public float centerProximity = 40;
 
     protected override void OnStart() {
     }
@@ -15,8 +16,20 @@ public class RandomPosition : ActionNode
     }
 
     protected override State OnUpdate() {
-        blackboard.moveToPosition.x = context.transform.position.x + Random.Range(min.x, max.x);
-        blackboard.moveToPosition.z = context.transform.position.z + Random.Range(min.y, max.y);
+        int willGoNearCenter = Random.Range(0,4);
+
+        if (willGoNearCenter == 0)
+        {
+            blackboard.moveToPosition.x = Random.Range(-centerProximity, centerProximity);
+            blackboard.moveToPosition.z = Random.Range(-centerProximity, centerProximity);
+        }
+
+        else
+        {
+            blackboard.moveToPosition.x = context.transform.position.x + Random.Range(min.x, max.x);
+            blackboard.moveToPosition.z = context.transform.position.z + Random.Range(min.y, max.y);
+        }
+        
         return State.Success;
     }
 }
