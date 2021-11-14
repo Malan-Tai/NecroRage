35a2d2211c8398d0b9561f450f3bd6db14feb6e8
Malan-Tai <malantai99@gmail.com>
2021-11-14 14:39:10 +0100

    tried fixing major bug

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -6,18 +6,27 @@ public class InputController : MonoBehaviour
 {
     private NecrovorePlayer _player;
 
+    [SerializeField]
+    private float _eatCooldown = 1f;
+    private float _curCooldown;
+
     void Start()
     {
         _player = this.GetComponent<NecrovorePlayer>();
+        _curCooldown = _eatCooldown;
     }
 
     void Update()
     {
+        _curCooldown += Time.deltaTime;
         _player.SetVelocity(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         if (Input.GetButtonDown("Jump")) _player.Dash();
 
-        if (Input.GetButtonDown("Eat")) _player.StartEating();
-        else if (Input.GetButtonUp("Eat")) _player.StopEating();
+        if (Input.GetButtonDown("Eat") && _curCooldown >= _eatCooldown) _player.StartEating();
+        else if (Input.GetButtonUp("Eat"))
+        {
+            if (_player.StopEating()) _curCooldown = 0f;
+        }
     }
 }
